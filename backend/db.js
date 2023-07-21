34a8e88e3cd04049ded7c80ed590b7022ca0fc7c
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-07-21 09:40:26 +0500

    pt 43 (project pt2)

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI ="mongodb://localhost:27017/?directConnection=true&readPreference=primary";
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to MongoDB");
+    });
+}
+// const a=console.log("aaa");
+module.exports=connectToMongo;
