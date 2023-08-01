a2ee8a7d92cbdc2f33b8e96e8545c52831a8e40c
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-08-01 19:51:28 +0500

    pt 50 (project pt 9)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -51,7 +51,8 @@ const authtoken = jwt.sign(data,JWT_SECRET);
     catch(error)
     {
       console.log(error.message);
-      res.status(600).send("some error occured");
+    res.status(500).send("interner server error");
+
     }
    
     //   .then(user => res.json(user)).catch(err=>{console.log(err)
@@ -64,5 +65,46 @@ const authtoken = jwt.sign(data,JWT_SECRET);
     // const user=User(req.body);
     // user.save();
     // res.send(req.body);
+})
+router.post('/login',[
+  body('email','Enter a valid email').isEmail(),
+  body('password','password cannot be blank').exists(),
+
+
+],async(req,res)=>{
+   // If there are errors, return bad request and error
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+   const {email,password}=req.body; //destructuring
+   try{
+    let user=await User.findOne({email});
+    if(!user)
+    {
+      return res.status(400).json({error:"sorry user doesnot exist"});
+    }
+    const passwordCompare=await bcrypt.compare(password,user.password);
+    if(!passwordCompare)
+    {
+      return res.status(400).json({error:"sorry user doesnot exist"});
+      
+    }
+   
+    const data={
+      user:{
+        id : user.id
+      }
+    }
+    const authtoken = jwt.sign(data,JWT_SECRET);
+      res.json({authtoken});
+    
+
+   }catch(error)
+   {
+    console.log(error.message);
+    res.status(500).send("interner server error");
+   }
+   
 })
 module.exports= router;
