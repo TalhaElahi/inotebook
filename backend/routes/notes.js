04d93ae77d4d936806e1ce3008276ce088292125
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-08-02 19:40:05 +0500

    pt 53 (project pt 12)

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -17,7 +17,7 @@ router.get('/fetchallnotes',fetchuser,async(req,res)=>{
 
     }
 });
-// ROUTE 1: Add a New Note using POST "/api/notes/addnote". Login Required
+// ROUTE 2: Add a New Note using POST "/api/notes/addnote". Login Required
 
 router.post('/addnote',fetchuser,[
     body('title','Enter a valid title').isLength({min:3}),
@@ -45,4 +45,26 @@ router.post('/addnote',fetchuser,[
     }
 })
 
+
+// ROUTE 3: Update an existing Note using PUT "/api/notes/updatenote". Login Required
+
+router.put('/updatenote/:id',fetchuser,async(req,res)=>{
+    const {title,description,tag}=req.body;
+    // Create New note object
+    const newNote={};
+    if (title){newNote.title=title};
+    if (description){newNote.description = description} ;
+    if (tag){newNote.tag = tag};
+
+    // Find the note to be updated and update it.
+    let note =await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("not found")}
+    if(note.user.toString() !== req.user.id)
+    {
+      return res.status(401).send("not allowed");
+    }
+    note=await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+    res.send(note);
+
+});
 module.exports= router;
