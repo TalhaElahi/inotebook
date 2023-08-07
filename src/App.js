bee32bf6da8a96a7950e2499baaa2739b0d05f09
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-08-07 21:02:33 +0500

    pt 57/58 (project pt 16 )

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import './App.css';
-import { Navbar } from './components/Navbar';
-import { Home } from './components/Home';
-import { About } from './components/About';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import NoteState from "./context/notes/NoteState";
 // import {
 //   createBrowserRouter,
 //   RouterProvider,
 // } from "react-router-dom";
 
-
 function App() {
   return (
     <>
-    <div className="App">
-<BrowserRouter>
-<Navbar/>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
 
-<Routes>
-<Route path="/" element={<Home/>}/>
-<Route path="/about" element={<About/>}/>
-
-
-</Routes>
-
-</BrowserRouter>
-      
-    </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import noteContext from "../context/notes/NoteContext";
 
 export const About = () => {
+  const a = useContext(noteContext);
+
+  useEffect(() => {
+    a.update();
+  }, []);
   return (
-    <div>This is About</div>
-  )
-}
+    <div>
+      This is About {a.state.name} and class is {a.state.class}
+    </div>
+  );
+};
