f8894fb4a95523bd0d846378f133204414de8dde
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-08-09 15:12:44 +0500

    pt 61 (project pt 19)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/NoteContext";
 
+import Notes from "./Notes";
 export const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context;
+
   return (
     <div>
       <div className="container">
@@ -49,11 +47,9 @@ export const Home = () => {
         </form>
       </div>
 
-      <div className="container my-3"></div>
-      <h2>Your Notes</h2>
-      {notes.map((note) => {
-        return note.title;
-      })}
+<Notes/>
+
+
     </div>
   );
 };
