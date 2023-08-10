bffaf0caa3d9f06e83926d1b705ad4f0aed5985a
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-08-10 19:41:10 +0500

    pt63 (project pt21)

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,19 +1,24 @@
 import React, { useContext } from "react";
+
 import noteContext from "../context/notes/NoteContext";
 import Noteitem from "./Noteitem";
+import AddNote from "./AddNote";
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const {notes, setNotes} = context;
-  return (
-       <div className="row my-3">
-      <h2>Your Notes</h2>
-      {notes.map((note) => {
-        return <Noteitem key={note.id} note={note}/>;
-      })}
-      </div>
-    
-  )
-}
+  const { notes,addNote } = context;
 
-export default Notes
+  return (
+    <>
+      <AddNote />
+      <div className="row my-3">
+        <h2>Your Notes</h2>
+        {notes.map((note) => {
+          return <Noteitem key={note._id} note={note} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Notes;
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import { useState } from "react";
 import NoteContext from "./NoteContext";
 const NoteState = (props) => {
   // const s1 = {
@@ -17,7 +17,7 @@ const NoteState = (props) => {
 
   const notesInitial = [
     {
-      id: "7664ca5b98fed820d425c1da3220",
+      _id: "7664ca5b98fed820d425c1da3220",
       user: "64c908dee7e7d746dc792ca23",
       title: "New Notes",
       description: "Please acces the playlist",
@@ -26,7 +26,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      id: "0064ca6915f87686b2fa18f1ec1f",
+      _id: "0064ca6915f87686b2fa18f1ec1f",
       user: "64c908dee7e7d74dc1792ca23",
       title: "New Notes3",
       description: "Please subscribe the channel",
@@ -35,7 +35,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      id: "9964ca6915f4868336b2fa8f1ec1f",
+      _id: "9964ca6915f4868336b2fa8f1ec1f",
       user: "64c908dee7e733d74dc792ca23",
       title: "New Notes3",
       description: "Please subscribe the channel",
@@ -44,7 +44,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      id: "8864ca6915f867786b2gfa8f1ec1f",
+      _id: "8864ca6915f867786b2gfa8f1ec1f",
       user: "64c908dee7e777d74dc792ca23",
       title: "New Notes3",
       description: "Please subscribe the channel",
@@ -53,7 +53,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      id: "7764ca69d15f868600b2fa8f1ec1f",
+      _id: "7764ca69d15f868600b2fa8f1ec1f",
       user: "64c908dee007e7d74dc792ca23",
       title: "New Notes3",
       description: "Please subscribe the channel",
@@ -62,14 +62,15 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      id: "2164ca69a15fa348686b2fa8f1ec1f",
+      _id: "2164ca69a15fa348686b2fa8f1ec1f",
       user: "64c908dee734e7d74dc792ca23",
       title: "New Notes3",
       description: "Please subscribe the channel",
       tag: "youtube",
       date: "2023-08-02T14:32:53.935Z",
       __v: 0,
-    },{
+    },
+    {
       _id: "ss64ca6915f8665486bs2fa8f1ec1f",
       user: "64c908d222ee7e7d74dc792ca23",
       title: "New Notes3",
@@ -77,8 +78,9 @@ const NoteState = (props) => {
       tag: "youtube",
       date: "2023-08-02T14:32:53.935Z",
       __v: 0,
-    },{
-      id: "aa64ca6915f86654d86b2fa8f1ec1f",
+    },
+    {
+      _id: "aa64ca6915f86654d86b2fa8f1ec1f",
       user: "64c908dee7e6547d74dc792ca23",
       title: "New Notes3",
       description: "Please subscribe the channel",
@@ -89,8 +91,36 @@ const NoteState = (props) => {
   ];
   const [notes, setNotes] = useState(notesInitial);
 
+  // Add a Note
+
+  const addNote = (title, description, tag) => {
+    // TODO API call
+console.log("adding a new note");
+   const note = {
+      "_id": "64ca6915f8686b2fa8f1ec1f",
+      "user": "64c908dee7e7d74dc792ca23",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2023-08-02T14:32:53.935Z",
+      "__v": 0
+    }
+    setNotes(notes.concat(note));
+  };
+
+
+  // Delete a Note
+  const deleteNote = () => {};
+
+
+
+  // Edit a Note
+  const editNote = () => {};
+
   return (
-    <NoteContext.Provider value={{notes,setNotes}}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{ notes, addNote,deleteNote,editNote }}>
+      {props.children}
+    </NoteContext.Provider>
   );
 };
 export default NoteState;
