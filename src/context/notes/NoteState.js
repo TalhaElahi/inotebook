9901f489cadfa0d1ef1ec1b27b6fa27bf56b460a
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2023-08-15 17:16:10 +0500

    pt 66 (project pt 24)

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -150,7 +150,19 @@ console.log("adding a new note");
 
 
   // Delete a Note
-  const deleteNote = (id) => {
+  
+  const deleteNote = async(id) => {
+
+     // API call
+     const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE", 
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRjOTA4ZGVlN2U3ZDc0ZGM3OTJjYTIzIn0sImlhdCI6MTY5MDkwODIyNn0._eb2EDFRkc4Z2PZu_dx1eMq7d9SNMOUeEagGooqYof0"
+      },
+    });
+    const json= response.json(); // parses JSON response into native JavaScript objects
+
     console.log("deleting the note with id "+id);
    const newNotes=notes.filter((note)=>{return note._id!==id});
    setNotes(newNotes);
