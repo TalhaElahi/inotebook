d0dc02268ed3dad0e5985bceb0b0ab600bbf9ca1
Talha Arif <60745973+TalhaElahi@users.noreply.github.com>
2024-01-25 22:58:34 +0500

    complete inote book

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI ="mongodb://localhost:27017/inotebook?directConnection=true&readPreference=primary";
+// const mongoURI ="mongodb://localhost:27017/inotebook?directConnection=true&readPreference=primary";
+mongoURI="mongodb+srv://kadaj_007:talha12345@cluster1.bjafdnd.mongodb.net/?retryWrites=true&w=majority";
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
         console.log("Connected to MongoDB");
